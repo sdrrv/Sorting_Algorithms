b43a98afc4f677c29e97cb974a80f13f6455a067
sdrrv <duartecelvas@gmail.com>
2020-07-26 11:45:10 +0100

    Buble Sort added

---
+++ Bubble_Sort.py
@@ -0,0 +1,8 @@
+class BubbleSort:
+    def __init__(self,list_to_order):
+        return sort(list_to_order)
+
+    def sort(self,list_to_order):
+        result=list_to_order
+        return result
+        
