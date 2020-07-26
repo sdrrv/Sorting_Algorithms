4b565313a7fd5e8798b12b84230466c3d46b3207
sdrrv <duartecelvas@gmail.com>
2020-07-26 21:28:39 +0100

    bubble sort updated

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -1,8 +1,8 @@
 class BubbleSort:
     def __init__(self,list_to_order):
-        return sort(list_to_order)
+        return self.sort(list_to_order)
 
     def sort(self,list_to_order):
         result=list_to_order
         return result
-        
+    
