c42675df237b7903ffd26481d984fc2a4be18eff
sdrrv <duartecelvas@gmail.com>
2020-07-26 23:34:43 +0100

    Bubble Sort v1

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -3,6 +3,17 @@ class BubbleSort:
         return self.sort(list_to_order)
 
     def sort(self,list_to_order):
-        result=list_to_order
-        return result
+        while True:
+            sorting= False
+            for i in range(len(list_to_order)-1):
+                if list_to_order[i+1] < list_to_order[i]:
+                    x=list_to_order[i]
+                    list_to_order[i]=list_to_order[i+1]
+                    list_to_order[i+1]=x
+                    sorting=True
+            if sorting:
+                break
+
+        return list_to_order
+
     
