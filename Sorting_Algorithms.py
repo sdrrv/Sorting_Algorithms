f0a0f56cefd41ba9292cf41820bb7709007f7aa8
sdrrv <duartecelvas@gmail.com>
2020-07-31 15:03:18 +0100

    Insertion Sort, added

--- Sorting_Algorithms.py
+++ Sorting_Algorithms.py
@@ -16,16 +16,22 @@ class SortingAlgorithms:
             if not sorting:
                 return list_to_order
     
-    def insertion_sort(list_to_order):
+    def insertion_sort(self,list_to_order):
         if len(list_to_order)<=1:
             return list_to_order
-
         else:
             for i in range(1,len(list_to_order)):
-                if list_to_order[i-1] > list_to_order[i]:
-                    x=list_to_order[i]
-                    list_to_order[i]=list_to_order[i-1]
-                    list_to_order[i-1]=x
+                k=i
+                element = list_to_order[i] #Ana Moura Santos
+                while k>0 and element < list_to_order[k-1]:
+                    list_to_order[k]=list_to_order[k-1]
+                    list_to_order[k-1]= element
+                    k-=1
+            return list_to_order
+            
+        
+                
+
                     
                 
 
